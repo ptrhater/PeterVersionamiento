3aa3efbff0c00e111455b11bf4ec1c853d6ba869
Peter Cruz <im.petercruz@gmail.com>
2014-08-13 18:59:33 -0600

    Trabajo

---
+++ Ejercicio1/Pagina1.php
@@ -0,0 +1,11 @@
+<html>
+    <head> Prueba de operaciones </head>
+    <form method="post" action="pagina2.php">
+        <body> 
+            <input type="checkbox" name="seleccion">
+            
+        
+        
+        </body>
+    </form>
+</html>
